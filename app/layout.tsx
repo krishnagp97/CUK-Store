fbaa11d9a3f83741ca89a69547601b11fef2678e
krishnagp97 <krishnapathak4511@gmail.com>
2026-08-16 12:22:16 +0530

    Add PWA support and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "CUK Store",
   description: "Buy and Sell Marketplace",
+  icons: {
+    icon: [
+      {
+        url: "/icons/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: "/icons/icon-192.png",
+  },
 };
 
 export const viewport: Viewport = {
