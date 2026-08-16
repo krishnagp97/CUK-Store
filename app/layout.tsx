0a1aaed070c88b713f29cf154d5eb848cf5cc1ae
krishnagp97 <krishnapathak4511@gmail.com>
2026-08-16 16:43:06 +0530

    fix PWA icons and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,18 +29,6 @@ export const metadata: Metadata = {
   title: "CUK Store",
   description: "Buy and Sell Marketplace",
   icons: {
-    icon: [
-      {
-        url: "/icons/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/icons/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
     apple: "/icons/icon-192.png",
   },
 };
